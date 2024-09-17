d3838c8d8616f66e9030df7101d29256c1196a3d
tapsu12 <thanhthuy190503@email.com>
2024-09-17 18:12:14 +0700

    Initial commit

---
+++ lib/main.dart
@@ -0,0 +1,54 @@
+import 'dart:math';
+import 'package:flutter/material.dart';
+
+void main() => runApp(
+  const MaterialApp(
+    home: MagicBall(),
+  ),
+);
+class MagicBall extends StatelessWidget {
+  const MagicBall({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      backgroundColor: Colors.blue,
+      appBar: AppBar(
+        backgroundColor: Colors.blue.shade900,
+        title: const Text('Ask Me Anything'),
+      ),
+      body: const Ball(),
+    );
+  }
+}
+
+class Ball extends StatefulWidget {
+  const Ball({super.key});
+
+  @override
+  State<Ball> createState() => _BallState();
+}
+
+class _BallState extends State<Ball> {
+  int ballNumber = 1;
+  @override
+  Widget build(BuildContext context) {
+    return Center(
+      child: TextButton(
+        onPressed: () {
+          setState(() {
+            ballNumber = Random().nextInt(5) + 1;
+          });
+        },
+        child: Image.asset('images/ball$ballNumber.png'),
+      ),
+    );
+  }
+
+}
+
+
+
+
+
+
